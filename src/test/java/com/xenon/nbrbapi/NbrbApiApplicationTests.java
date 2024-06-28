243bc18323bcd4abf652d9ae2c08d19425c891b9
savva <savva.roll@gmail.com>
2024-06-28 19:51:41 +0300

    Initial commit

---
+++ src/test/java/com/xenon/nbrbapi/NbrbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenon.nbrbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbrbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
