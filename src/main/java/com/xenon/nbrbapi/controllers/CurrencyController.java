505057e8392c31702dfdfad255bbdf21b3726607
savva <savva.roll@gmail.com>
2024-06-29 15:06:45 +0300

    Removed excess controllers

--- src/main/java/com/xenon/nbrbapi/controllers/CurrencyController.java
+++
@@ -1,9 +0,0 @@
-package com.xenon.nbrbapi.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/currency")
-public class CurrencyController {
-}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,22 @@
 create table rate
 (
-    curId           int PRIMARY KEY not null,
-    date            date            not null,
-    curAbbreviation varchar(255)    not null,
-    curScale        int             not null,
-    curName         varchar(255)    not null,
-    curOfficialRate decimal         not null
+    curId           int          not null,
+    date            date         not null,
+    curAbbreviation varchar(255) not null,
+    curScale        int          not null,
+    curName         varchar(255) not null,
+    curOfficialRate decimal      not null
 );
 
 create table currency
 (
-    curId           int PRIMARY KEY not null,
-    curParentId     int             not null,
-    curCode         varchar(255)    not null,
-    curAbbreviation varchar(255)    not null,
-    curName         varchar(255)    not null,
-    curNameEng      varchar(255)    not null,
-    curScale        int             not null,
-    curPeriodicity  int             not null
+    rowId           int auto_increment primary key not null,
+    curId           int          not null,
+    curParentId     int          not null,
+    curCode         varchar(255) not null,
+    curAbbreviation varchar(255) not null,
+    curName         varchar(255) not null,
+    curNameEng      varchar(255) not null,
+    curScale        int          not null,
+    curPeriodicity  int          not null
 );
